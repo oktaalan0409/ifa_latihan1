9dbe0dbdccdf75f7b022b408ae3dc5300f6ed430
PC-16 Bisnis Digital <pc-16bisnisdigital@PC-16s-iMac.local>
2023-10-12 10:43:16 +0700

    commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:collection';
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+      Title: "latihan 1"
+      home: Scaffold()  
+        appBar: AppBar
+          title: Text("Latihan 1")
+          backgroundColor: Colors.black,
+        ),
+      
+         Text("Universitas Teknokrat Indonesia"),
+)
+}
